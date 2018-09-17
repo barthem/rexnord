023919a87e9a85d0485c2ea5a8d8d03d1d729454
barthem <bart-de-langen@hotmail.com>
2018-09-17 15:01:49 +0200

    added even more example files to use sockets

---
+++ code samples/socket_clientTest.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import socket
+
+HOST = '127.0.0.1'  # The server's hostname or IP address
+PORT = 12345        # The port used by the server
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+
+    print(s)
+    data = input("input to send?")
+    s.send(data.encode())
+    s.close()
+    # while(True):
+    #     data = input("input?")
+    #     s.send(data)
